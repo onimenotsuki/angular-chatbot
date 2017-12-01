c2cbe50a3308180cc0b73bcc257835d045e88303
Edgar Talledos <onimenotsuki@gmail.com>
2017-12-01 08:38:29 -0600

    Add env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: 'f1c0a3984f864a6d95eab21f68341128'
+  }
 };
