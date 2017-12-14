37590dc40d57d3f27097efba3982a103c5d42476
Edgar Talledos <onimenotsuki@gmail.com>
2017-12-14 17:52:54 -0600

    Add env vars to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@ export const environment = {
   production: true,
   dialogflow: {
     angularBot: '8bae111177264b018647e31b772ba9a4'
+  },
+  firebase: {
+    apiKey: 'AIzaSyBi6SpjTw9PJPOKkjYsJEXPVQXbBqR-DUE',
+    authDomain: 'firechat-350ed.firebaseapp.com',
+    databaseURL: 'https://firechat-350ed.firebaseio.com',
+    projectId: 'firechat-350ed',
+    storageBucket: 'firechat-350ed.appspot.com',
+    messagingSenderId: '321548219234'
   }
 };
